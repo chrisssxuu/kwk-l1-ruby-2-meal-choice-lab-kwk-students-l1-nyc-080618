4124773aecf4765a1d61aad690409c73685109aa
chrisssxuu <chris01.xu@gmail.com>
2018-08-07 14:56:37 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -5,18 +5,17 @@ end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
 
-puts "What food do you want to eat for breakfast?"
-morning = gets.chomp
-
 def breakfast(morning="frosted flakes")
   "Morning is the best time for #{morning}"
 end
 
+def lunch(afternoon="grilled cheese")
+  "Lunch is the best time for #{afternoon}"
+end
 
-
-
-
-
+def dinner(night="grilled cheese")
+  "Dinner is the best time for #{night}"
+end
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
